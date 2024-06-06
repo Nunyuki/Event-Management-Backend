12bea10712ab793d8314fd5d1fe1e848bdd4400f
Nunyuki <cdaielodie@gmail.com>
2024-06-06 20:41:03 +0200

    fix bug with sql & add WebConfig

--- src/main/java/com/dauphine/event_management_backend/controllers/EventUserController.java
+++ src/main/java/com/dauphine/event_management_backend/controllers/EventUserController.java
@@ -1,5 +1,4 @@
 package com.dauphine.event_management_backend.controllers;
-
 import org.springframework.web.bind.annotation.*;
 
 @RestController
--- src/main/java/com/dauphine/event_management_backend/services/impl/EventUserServiceImpl.java
+++ src/main/java/com/dauphine/event_management_backend/services/impl/EventUserServiceImpl.java
@@ -31,4 +31,6 @@ public class EventUserServiceImpl implements EventUserService {
     public void deleteById(UUID id) {
 
     }
+
+
 }
